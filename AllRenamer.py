b2fc7e877f5ef8d879fa5a9ffe0dfa16fcc5657b
Дмитрий Добрышин <dimkainc@mail.ru>
2022-06-17 15:20:06 +0300

    Модификация до версии 1.1.0.32. Изменён алгоритм учёта дубликатов и контроля дубликатов. В результате просходит два цикла обработки файлов

--- AllRenamer.py
+++ AllRenamer.py
@@ -14,10 +14,10 @@
 import os, zipfile, datetime, zlib
 from termcolor import colored
 from colorama import init
-#initializes Colorama
-init(autoreset=True)
-now = datetime.datetime.now()
+# Инициализация Colorama
+init(autoreset = True)
 
+now = datetime.datetime.now()
 version = "1.1.0.32"
 author = "Дмитрий Добрышин"
 email = "dimkainc@mail.ru"
@@ -29,6 +29,8 @@ print(colored("©", "yellow", attrs = ["bold"]),
   colored(email, "cyan", attrs = ["bold", "underline"]), "\n")
 
 files = os.listdir(".")
+lenFiles = len(files)
+files.sort()
 crc32list = []
 
 def crc32(filename, chunksize=65536):
@@ -42,28 +44,54 @@ def crc32(filename, chunksize=65536):
 def existfile(nfile, ext):
   """Генерация имени фала, если совпадает с существующим"""
   global crc32list
-  crc32list = []
+
   num = 0
   newfile = nfile + ext
   while os.path.exists(newfile):
-    crc32list.append(crc32(newfile))
     num += 1
     newfile = nfile + "(%d)" % num + ext
   return newfile
 
+#allFilesCRC32 = []
+"""Сбор CRC32 всех файлов"""
+print(colored("Производится учёт всех существующих файлов для исключения дубликатов", "grey"))
+percent = 0
 for file in files:
+  percent += 1
+  print( "%d" % (percent * 100 // lenFiles), "%", end="\r")
+  if (file.lower().endswith(".fb2.zip") or 
+    (file.lower().endswith(".epub") and 
+    not file.lower().endswith(".fb2.epub")) or 
+    file.lower().endswith(".fb2")):
+    fcrc32 = crc32(file)
+
+    if fcrc32 in crc32list:
+      os.remove(file)
+      print(colored("[ДУБЛИКАТ]", "red", attrs = ["bold"]), "Удалён файл: " + file)
+    else:
+      crc32list.append(fcrc32)
+
+files = os.listdir(".")
+lenFiles = len(files)
+files.sort()
+percent = 0
+
+for file in files:
+  percent += 1
+  print( "%d" % (percent * 100 // lenFiles), "%", end="\r")
   ext = ".fb2.epub"
   newext = ".epub"
   endext = len(ext)
   num = 0
   if file.lower().endswith(ext):
     mycrc32 = crc32(file)
-    newfile = existfile(file[0:-endext], newext)
     if mycrc32 in crc32list:
       os.remove(file)
       print(colored("[EPUB]", "red", attrs = ["bold"]), "Это дубликат: " +
         file)
     else:
+      crc32list.append(mycrc32)
+      newfile = existfile(file[0:-endext], newext)
       os.rename(file, newfile)
       print(colored("[EPUB]", "yellow", attrs = ["bold"]), "Файл переименован: " +
         newfile)
@@ -80,6 +108,7 @@ for file in files:
       print(colored("[FB2.ZIP]", "red", attrs = ["bold"]), "Это дубликат: " +
         file)
     else:
+      crc32list.append(mycrc32)
       print(colored("[FB2.ZIP]", "green", attrs = ["bold"]), "Файл сжат: " +
         newfile)
  
