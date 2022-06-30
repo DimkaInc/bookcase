30c796dd84c0ee3074d51ed492c3bc9b7f2b51fa
Добрышин Дмитрий <dimkainc@mail.ru>
2022-06-30 18:44:24 +0300

    Добавлен тип файлов .mobi

--- AllRenamer.py
+++ AllRenamer.py
@@ -188,7 +188,7 @@ class GoodBooks:
         while( filename := self.fileList.getNextFile()):
             # Необходимо вычленить файлы, которые одинаково называются без версий
             dfile = self.fileList.getFileStruct(self.fileList.getFullPath(filename))
-            if not dfile.get("extension") in [".fb2", ".zip", ".epub", ".fb2.zip", ".fb2.epub"]:
+            if not dfile.get("extension") in [".fb2", ".zip", ".epub", ".fb2.zip", ".fb2.epub", ".mobi"]:
                 continue
             shortName = dfile.get("clearFileName") + dfile.get("extension")
             crc = dfile.get("crc32")
